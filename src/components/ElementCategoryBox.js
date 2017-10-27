b9b3acad836279f3c5b92a5ca17e7d87079f2aed
Carlos Martinez <itscarlosisaac@gmail.com>
2017-10-27 12:01:26 -0400

    Added CSS to the element category section.

--- src/components/ElementCategoryBox.js
+++ src/components/ElementCategoryBox.js
@@ -13,27 +13,31 @@ class ElementCategoryBox extends React.Component{
         return(
             <div className="element__category__box">
                 <div className="element__category__metals">
+                    <h2 className="element__category__title">METALS</h2>
                     <div>
                         {
                             metals.map( (metal) => {
-                                return <p key={metal} className={metal}>{metal} </p>
+                                return <p key={metal} className={`element__cat ${metal.toLowerCase().replace(/-| /g, '_')}`}>{metal} </p>
                             })
                         }
                     </div>
                 </div>
-                <div className="element__category__metaloids">
-                    <div> 
-                        <p className={metalloid}>{metalloid} </p>
+                <div className="element__category__section">
+                    <div className="element__category__metaloids">
+                    <h2 className="element__category__title">METALOIDS</h2>
+                        <div> 
+                            <p className={`element__cat ${metalloid.toLowerCase()}`}>{metalloid} </p>
+                        </div>
                     </div>
-                </div>
-                <div className="element__category__nonmetals">
-                    <h2>NON-METALS</h2>
-                    <div>
-                        {
-                            nonmetals.map( (nonmetal) => {
-                                return <p key={nonmetal} className={nonmetal}>{nonmetal} </p>
-                            })
-                        }
+                    <div className="element__category__nonmetals">
+                        <h2 className="element__category__title">NON-METALS</h2>
+                        <div>
+                            {
+                                nonmetals.map( (nonmetal) => {
+                                    return <p key={nonmetal} className={`element__cat ${nonmetal.toLowerCase().replace(/-| /g, '_')}`}>{nonmetal} </p>
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
