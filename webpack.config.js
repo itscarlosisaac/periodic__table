17d90af2e2ae7dd22107429869a1d96a65e813b3
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-12 00:13:03 -0400

    Added  webpack for production.

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,38 @@
 // Webpack configuration file 
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist')
+module.exports = (env) =>  {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css') // Name of the output file extracted
+    return { 
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            historyApiFallback: true,
+            contentBase: path.join(__dirname, 'dist')
+        }
     }
 }
